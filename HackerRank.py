8f0e929806745919a54b2f0d320b7bb4a4438eda
thientantran <thientantm@gmail.com>
2021-02-02 20:45:58 +0700

    2.2

--- HackerRank.py
+++ HackerRank.py
@@ -1,72 +1,72 @@
-# ############# ---------------------function()
+############# ---------------------function()
 
-# def is_leap(year):
-#     leap = False
-#     if year %4==0:
-#         leap=True
-#         if year%400==0: 
-#             leap=True
-#         elif year%100==0 and year%400!=0:
-#             leap=False
-#     else:
-#         leap=False
-#     return leap
+def is_leap(year):
+    leap = False
+    if year %4==0:
+        leap=True
+        if year%400==0: 
+            leap=True
+        elif year%100==0 and year%400!=0:
+            leap=False
+    else:
+        leap=False
+    return leap
 
-# year = int(input())
-# print(is_leap(year))
+year = int(input())
+print(is_leap(year))
 
-# ###### ------------------------------itertools.combinations()
+###### ------------------------------itertools.combinations()
 
-# from itertools import combinations
+from itertools import combinations
 
-# s , n  = input().split()
+s , n  = input().split()
 
-# for i in range(1, int(n)+1):
-#     for j in combinations(sorted(s), i):
-#         #sorted(s) trả về list
-#         print(''.join(j))
-#         # join sử dụng cho list
+for i in range(1, int(n)+1):
+    for j in combinations(sorted(s), i):
+        #sorted(s) trả về list
+        print(''.join(j))
+        # join sử dụng cho list
 
-#####----------------List Comprehensions------------------
+####----------------List Comprehensions------------------
 
-# x = int(input())
-# y = int(input())
-# z = int(input())
-# n = int(input())
-# A=[]
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+A=[]
 
-# for i in range(x+1):
-#     a=[0,0,0]
-#     a[0]=i
-#     for j in range(y+1):
-#         a[1]=j
-#         for k in range(z+1):
-#             a[2]=k    
-#             A.append(a)
-#             # tại sao khi thay đổi a thì A lại thay đổi theo a        
-# kq=[]
-# for i in range(len(A)):
-#     if sum(A[i])!=n:
-#         kq.append(A[i])
-# print(kq)
+for i in range(x+1):
+    a=[0,0,0]
+    a[0]=i
+    for j in range(y+1):
+        a[1]=j
+        for k in range(z+1):
+            a[2]=k    
+            A.append(a)
+            # tại sao khi thay đổi a thì A lại thay đổi theo a        
+kq=[]
+for i in range(len(A)):
+    if sum(A[i])!=n:
+        kq.append(A[i])
+print(kq)
 
-# #answer
-# print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
+#answer
+print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
 
-###################-----------------Find the Runner-Up Score!----------------
-# n = int(input())
-# arr = map(int, input().split())
-# arr=list(arr)
-# max=-101
-# for i in range(len(arr)):
+##################-----------------Find the Runner-Up Score!----------------
+n = int(input())
+arr = map(int, input().split())
+arr=list(arr)
+max=-101
+for i in range(len(arr)):
     
-#     if max<arr[i]:
-#         max=arr[i]
-# kq=-101
-# for i in range(len(arr)):
-#     if kq<arr[i] and arr[i]<max:
-#         kq=arr[i]
-# print(kq)
+    if max<arr[i]:
+        max=arr[i]
+kq=-101
+for i in range(len(arr)):
+    if kq<arr[i] and arr[i]<max:
+        kq=arr[i]
+print(kq)
 
 ######---------------------------------Nested Lists
 
